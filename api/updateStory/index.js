f3581eedff1cba102615eaa270d88f66e78a5aee
Jen Looper <jen.looper@gmail.com>
2020-09-04 10:59:38 -0400

    update...almost working, not quite

--- api/updateStory/index.js
+++ api/updateStory/index.js
@@ -1,38 +1,19 @@
-const CosmosClient = require('@azure/cosmos').CosmosClient;
-
-function validateStory(story) {
-	const updatedStory = {
-		title: story.title,
-		id: story.id,
-	};
-
-	return updatedStory;
-}
-
-module.exports = async function (context, req) {
-	var cosmos_config = {};
-	cosmos_config.endpoint = process.env['VUE_APP_COSMOS_ENDPOINT'];
-	cosmos_config.key = process.env['VUE_APP_COSMOS_KEY'];
-	const client = new CosmosClient(cosmos_config);
-	const databaseId = 'storytelling';
-	const containerId = 'stories';
-
-	const database = client.database(databaseId);
-	const container = database.container(containerId);
-
-	try {
-		const story = req.body;
-		const updatedStory = validateStory(story);
-		console.log(story.id);
-		const storyRes = await container.item(story.id, story.title).replace(updatedStory);
-
+module.exports = function (context, req) {
+	if (req.body && req.body) {
+		context.bindings.story = JSON.stringify({
+			id: req.body.id,
+			title: req.body.title,
+			item: req.body.item,
+		});
+		context.done();
 		context.res = {
-			body: { ...storyRes.resource },
+			status: 200,
+			body: req.body,
 		};
-	} catch (error) {
+	} else {
 		context.res = {
-			status: 500,
-			body: error.message,
+			status: 400,
+			body: 'Please pass a story in the request body',
 		};
 	}
 };
