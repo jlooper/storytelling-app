c57b5a2e76e559da0389ed53422b8d7c7f8040be
softchris <noringc@gmail.com>
2020-09-09 12:50:21 +0100

    adding blob binding

--- api/uploadStoryImage/index.js
+++ api/uploadStoryImage/index.js
@@ -1,25 +1,29 @@
-module.exports = async function (context, req) {
-	const image = req.body;
-	//console.log(req);
-	const { BlobServiceClient } = require('@azure/storage-blob');
-	const { v1: uuidv1 } = require('uuid');
-	const blobServiceClient = BlobServiceClient.fromConnectionString(process.env['VUE_APP_STORAGE_CONNECTION_STRING']);
-	const containerName = 'images';
-	const containerClient = blobServiceClient.getContainerClient(containerName);
-	const blobName = uuidv1() + '.png';
-	const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+// @ts-check
+const formdata = require("multipart-formdata");
 
-	/*const getStream = require('into-stream');
+module.exports = (context, req) => {
+	try {
 
-	console.log('\nUploading to Azure storage as blob:\n\t', blobName);
-	const buff = Buffer.from(image, 'base64');
-	const stream = getStream(buff);
-	console.log(stream);
-	const streamLength = buff.length;
-	const uploadBlobResponse = await blockBlobClient.uploadStream(stream, streamLength);
-	console.log('Blob was uploaded successfully. requestId: ', uploadBlobResponse.requestId);
-	*/
-	const data = image;
-	const uploadBlobResponse = await blockBlobClient.upload(data, data.length);
-	console.log('Blob was uploaded successfully. requestId: ', uploadBlobResponse.requestId);
+		if (req.body) {
+			const boundary = formdata.getBoundary(req.headers["content-type"]);
+			const [file] = formdata.parse(req.body, boundary)
+
+			context.bindings.storage = file.data;
+			context.done();
+		} else {
+			return endWithBadResponse(context, `Request Body is not defined`);
+		}
+	} catch (err) {
+		context.log.error(err.message);
+		throw err;
+	}
 };
+
+function endWithBadResponse(context, message = "Bad Request") {
+	context.log.error(message);
+	context.bindings.response = {
+		status: 400,
+		body: message,
+	};
+	context.done();
+}
