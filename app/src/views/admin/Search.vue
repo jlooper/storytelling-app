2b119d9a238ec0e31ca9542f060f2f0dc753d7e5
Jen Looper <jen.looper@gmail.com>
2020-09-15 12:18:40 -0400

    refactoring CH component

--- app/src/views/admin/Search.vue
+++ app/src/views/admin/Search.vue
@@ -25,133 +25,108 @@
 		<section class="is-pulled-right mb-20">
 			<button @click="goToStoryCustomizer()" class="button is-link">Continue</button>
 		</section>
-		<div class="is-pulled-left">
-			<div class="box card m-5" v-for="(object, index) in CooperHewittMuseumObjects" :key="index">
+
+		<section>
+			<hr />
+			<p class="is-size-4 has-text-left has-text-link">Story box</p>
+
+			<draggable class="drop-box" v-model="myStories"></draggable>
+			<p class="is-size-4 has-text-left has-text-link">Drag cards to story box to create your story</p>
+		</section>
+
+		<CooperHewitt />
+
+		<!--VA-->
+
+		<div>
+			<div class="box card m-5" v-for="(object, index) in VAObjects" :key="index">
 				<div class="card-content">
 					<div class="media">
 						<div class="media-left">
-							<figure class="image is-128x128" v-for="(o, index) in object.images" :key="index">
-								<img v-if="index == 0" :src="o.b.url" :alt="object.title" />
-							</figure>
+							<!--<figure v-if="object.hasImage" class="image is-128x128" style="max-height: 100px">
+								<img :src="object.webImage.url" :alt="object.title" />
+							</figure>-->
+							<p v-if="object.fields.title != ''" class="title is-4">{{ object.fields.title }}</p>
+							<p v-if="object.fields.object != ''" class="subtitle is-6">{{ object.fields.object }}</p>
+							<p v-if="object.fields.date_text != ''" class="subtitle is-6">
+								Date: {{ object.fields.date_text }}
+							</p>
+							<p v-if="object.fields.location != ''" class="subtitle is-6">
+								Location: {{ object.fields.location }}
+							</p>
+							<p v-if="object.fields.artist != ''" class="subtitle is-6">
+								Artist: {{ object.fields.artist }}
+							</p>
+							<p v-if="object.fields.place != ''" class="subtitle is-6">
+								Plact: {{ object.fields.place }}
+							</p>
 						</div>
 						<div class="media-content">
-							<p>
-								<a :href="object.url != ''" target="_blank" class="title is-4">{{ object.title }}</a>
-							</p>
-							<p v-if="object.medium != ''" class="subtitle is-6">{{ object.medium }}</p>
-							<p v-if="object.label_text != ''" class="subtitle is-6">{{ object.label_text }}</p>
-							<p v-if="object.accession_number != ''" class="subtitle is-6">
-								Accession Number: {{ object.accession_number }}
-							</p>
-							<p v-if="object.id" class="subtitle is-6">Object Id: {{ object.id }}</p>
-							<p v-if="object.creditline != ''">{{ object.creditline }}</p>
-							<br />
-							<time datetime="2016-1-1">Acquired {{ object.year_acquired }}</time>
-							<br />
-							<p class="tag is-primary mt-3">Cooper Hewitt Museum Collection</p>
+							<span class="tag is-dark mt-3">Victoria and Albert Collection</span>
 						</div>
-						<div class="field is-grouped">
+						<!--<div class="field is-grouped">
 							<div class="control">
-								<button class="button is-info" @click="createStoryItem(object)">
-									Add to Story
-								</button>
+								<button class="button is-info" @click="addToStory(object)">Add to Story</button>
 							</div>
-						</div>
+						</div>-->
 					</div>
 				</div>
 			</div>
 		</div>
-
-		<!--Rijks-->
-
-		<!--<div>
-			<div class="box card m-5" v-for="(object, index) in RijksMuseumObjects" :key="index">
-				<div class="card-content">
-					<div class="media">
-						<div class="media-left">
-							<figure v-if="object.hasImage" class="image is-128x128" style="max-height: 100px">
-								<img :src="object.webImage.url" :alt="object.title" />
-							</figure>
-						</div>
-						<div class="media-content">
-							<p>
-								<a :href="object.links.web != ''" target="_blank" class="title is-4">
-									{{ object.title }}
-								</a>
-							</p>
-							<p>Principal or First Maker: {{ object.principalOrFirstMaker }}</p>
-							<p v-for="p in object.productionPlaces" :key="p.id">{{ p }}</p>
-							<span class="tag is-dark mt-3">Rijksmuseum Collection</span>
-						</div>
-						<div class="field is-grouped">
-							<div class="control">
-								<button class="button is-info" @click="addToStory(object)">Add to Story</button>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>-->
 	</main>
 </template>
 <script>
 var axios = require('axios');
 import Steps from '@/components/Steps';
+import CooperHewitt from '@/components/Museums/CooperHewitt';
+import draggable from 'vuedraggable';
 export default {
 	components: {
 		Steps,
+		draggable,
+		CooperHewitt,
+	},
+	props: {
+		searchString: String,
 	},
 	data() {
 		return {
-			CooperHewittMuseumObjects: [],
-			RijksMuseumObjects: [],
+			//CooperHewittMuseumObjects: [],
+			VAObjects: [],
 			storyTitle: '',
 			message: '',
 			search_field: null,
 			isLoading: false,
 			isFullPage: false,
-
+			myStories: [],
 			storyId: '',
 		};
 	},
 	methods: {
 		async submit() {
+			this.$emit('search', this.search_field);
 			this.isLoading = true;
-			this.CooperHewittMuseumObjects = [];
-			this.RijksMuseumObjects = [];
-			var chconfig = {
+			//this.CooperHewittMuseumObjects = [];
+			this.VAObjects = [];
+
+			var vaconfig = {
 				method: 'get',
-				url: 'https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.search.objects',
-				headers: {},
+				url: 'https://www.vam.ac.uk/api/json/museumobject/search',
 				params: {
-					access_token: '5a3c42c653014805fd1e06902631596f',
-					query: this.search_field,
-					page: 1,
-					per_page: 10,
+					q: this.search_field,
 				},
 			};
 
-			/*var rijksconfig = {
-					method: 'get',
-					url: 'https://www.rijksmuseum.nl/api/nl/collection?key=PqVYN24g',
-					params: {
-						q: this.search_field,
-						p: 1,
-						ps: 10,
-						st: 'Objects',
-					},
-				};
-*/
-			const firstrequest = axios(chconfig).then((response) => {
+			/*const firstrequest = axios(chconfig).then((response) => {
 				this.CooperHewittMuseumObjects = response.data.objects;
+			});*/
+
+			const secondrequest = axios(vaconfig).then((response) => {
+				this.VAObjects = response.data.records;
 			});
 
-			/*const secondrequest = axios(rijksconfig).then((response) => {
-					this.RijksMuseumObjects = response.data.artObjects;
-				});*/
-
 			axios
-				.all([axios(firstrequest) /*, axios(secondrequest)*/])
+				.all([/*axios(firstrequest), */ axios(secondrequest)])
 				.then((this.isLoading = false))
 				.catch((error) => console.log(error));
 		},
@@ -167,7 +142,7 @@ export default {
 					if (response.data.errorMessage == null) {
 						this.$buefy.toast.open(response.data.message);
 					} else {
-						this.$buefy.toast.open(response.data.message);
+						this.$buefy.toast.open(response.data.errorMessage);
 					}
 				})
 				.catch(function(error) {
@@ -193,5 +168,11 @@ export default {
 .image img {
 	width: auto;
 	max-height: 200px;
+}
+.drop-box {
+	height: 200px;
+	margin: 10px;
+	background-color: #eee;
+	border-radius: 5px;
 }
 </style>
