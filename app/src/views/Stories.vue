b2a84aabcd0ebc0b4d82cda66abdca58841bd455
Jen Looper <jen.looper@gmail.com>
2020-09-20 17:52:30 -0400

    skeleton loader

--- app/src/views/Stories.vue
+++ app/src/views/Stories.vue
@@ -1,62 +1,68 @@
 <template>
-	<main class="column is-four-fifths main is-pulled-right">
-		<h1 class="title">Stories</h1>
-		<div class="main-content columns is-multiline">
-			<hr />
-			<div class="card column is-one-quarter" v-for="story in stories" :key="story.id">
-				<div class="card-image">
-					<figure class="image is-4by3">
-						<img :src="buildImageUrl(story.imageUrl)" :alt="story.title" />
-					</figure>
-				</div>
-				<div class="card-content">
-					<div class="media">
-						<div class="media-content">
-							<router-link :to="{ path: '/story/' + story.id + '' }" class="title is-5">{{
-								story.title
-							}}</router-link>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</main>
+  <main class="column is-four-fifths main is-pulled-right">
+    <h1 class="title">Stories</h1>
+    <div class="main-content columns is-multiline">
+      <hr />
+      <div class="card column is-one-quarter" v-for="story in stories" :key="story.id">
+        <div class="card-image" v-if="loaded">
+          <figure class="image is-4by3">
+            <img :src="buildImageUrl(story.imageUrl)" :alt="story.title" />
+          </figure>
+        </div>
+        <b-skeleton height="300px" :active="!loaded" :count="1"></b-skeleton>
+        <div class="card-content" v-if="loaded">
+          <div class="media">
+            <div class="media-content">
+              <router-link :to="{ path: '/story/' + story.id + '' }" class="title is-5">
+                {{
+                story.title
+                }}
+              </router-link>
+            </div>
+          </div>
+        </div>
+        <b-skeleton size="is-large" :active="!loaded" :count="3"></b-skeleton>
+      </div>
+    </div>
+  </main>
 </template>
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
-	data() {
-		return {
-			stories: [],
-		};
-	},
-	methods: {
-		buildImageUrl(url) {
-			//build the url for access to image
-			let string = url;
-			let imageName = string.substring(string.lastIndexOf('/') + 1);
-			return (
-				'https://cooperhewittstorage.blob.core.windows.net/images/' +
-				imageName +
-				'?sp=rl&st=2020-09-11T02:45:14Z&se=2021-12-12T02:45:00Z&sv=2019-12-12&sr=c&sig=ineO%2FmFbKg2t5nFyfg9AO%2Ft4wTDvDrKdBPoPjhA0%2F78%3D'
-			);
-		},
-	},
-	created() {
-		axios
-			.get('/api/getStories')
-			.then((response) => {
-				if (response.status === 200) {
-					this.stories = response.data;
-					console.log(this.stories);
-				} else {
-					this.uploadError = true;
-				}
-			})
-			.catch((err) => {
-				this.uploadError = true;
-				console.error(err);
-			});
-	},
+  data() {
+    return {
+      stories: [],
+      loaded: false,
+    };
+  },
+  methods: {
+    buildImageUrl(url) {
+      //build the url for access to image
+      let string = url;
+      let imageName = string.substring(string.lastIndexOf("/") + 1);
+      return (
+        "https://cooperhewittstorage.blob.core.windows.net/images/" +
+        imageName +
+        "?sp=rl&st=2020-09-11T02:45:14Z&se=2021-12-12T02:45:00Z&sv=2019-12-12&sr=c&sig=ineO%2FmFbKg2t5nFyfg9AO%2Ft4wTDvDrKdBPoPjhA0%2F78%3D"
+      );
+    },
+  },
+  created() {
+    axios
+      .get("/api/getStories")
+      .then((response) => {
+        this.loaded = true;
+        if (response.status === 200) {
+          this.stories = response.data;
+          console.log(this.stories);
+        } else {
+          this.uploadError = true;
+        }
+      })
+      .catch((err) => {
+        this.uploadError = true;
+        console.error(err);
+      });
+  },
 };
 </script>
