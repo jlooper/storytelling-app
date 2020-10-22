cb51f0341345d2d20b737629101d485948e52f80
Jen Looper <jen.looper@gmail.com>
2020-10-22 10:02:57 -0400

    removing references to dummy

--- app/src/main.js
+++ app/src/main.js
@@ -6,15 +6,6 @@ import buefy from 'buefy';
 
 Vue.use(buefy);
 
-import DummyRed		from '@/components/dummy/DummyRed.vue'
-import DummyGreen	from '@/components/dummy/DummyGreen.vue'
-import DummyBlue	from '@/components/dummy/DummyBlue.vue'
-
-Vue.component(DummyRed.name, DummyRed)
-Vue.component(DummyGreen.name, DummyGreen)
-Vue.component(DummyBlue.name, DummyBlue)
-
-
 Vue.config.productionTip = false;
 
 new Vue({
