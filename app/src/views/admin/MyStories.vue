ccec974020946b7506a3c730195eb1ee895e4905
Jen Looper <jen.looper@gmail.com>
2020-10-29 16:42:49 -0400

    adding a way to start a story w no title

--- app/src/views/admin/MyStories.vue
+++ app/src/views/admin/MyStories.vue
@@ -2,7 +2,13 @@
   <main class="column is-four-fifths main is-pulled-right">
     <h1 class="title">My Stories</h1>
     <StoryGrid v-bind:stories="myStories" v-bind:admin="true" />
-    <router-link class="button is-large is-link mt-5" :to="'create'">Create a Story</router-link>
+    <span
+      class="button is-large is-primary is-uppercase mx-4 my-5"
+      @click="createStory"
+      >Create a Story by Searching</span
+    ><router-link class="button is-large is-link mx-4 my-5" :to="'create'"
+      >Create a Story with a Title</router-link
+    >
   </main>
 </template>
 
@@ -33,6 +39,36 @@ export default {
         imageName +
         "?sp=rl&st=2020-09-11T02:45:14Z&se=2021-12-12T02:45:00Z&sv=2019-12-12&sr=c&sig=ineO%2FmFbKg2t5nFyfg9AO%2Ft4wTDvDrKdBPoPjhA0%2F78%3D"
       );
+    },
+    createStory() {
+      axios
+        .post("/api/createStory", {
+          title: "New Story",
+          imageUrl:
+            "https://cooperhewittstorage.blob.core.windows.net/images/no_image.jpg",
+          userId: this.userId,
+        })
+        .then((response) => {
+          this.isLoading = false;
+          if (response.data.errorMessage == null) {
+            this.$buefy.toast.open(response.data.message);
+
+            this.proceed = !this.proceed;
+            this.storyId = response.data.id;
+            this.storyTitle = response.data.title;
+            this.$router.push({
+              path: "/Search",
+              name: "search",
+              params: { id: this.storyId, title: this.storyTitle },
+            });
+          } else {
+            this.$buefy.toast.open(response.data.message);
+          }
+        })
+        .catch(function (error) {
+          this.isLoading = false;
+          console.log(error);
+        });
     },
   },
   created() {
--- app/src/views/admin/MyStory.vue
+++ app/src/views/admin/MyStory.vue
@@ -1,45 +1,49 @@
 <template>
-	<div class="columns">
-		<div class="column">
-			<h1 class="title">Story Elements</h1>
-			<!--breadcrumbs-->
-			<Breadcrumbs active="0" />
-			<StoryItemsGrid v-bind:storyItems="myStoryItems" v-bind:admin="true" />
-		</div>
-	</div>
+  <div class="columns">
+    <div class="column">
+      <h1 class="title">Story Elements</h1>
+      <!--breadcrumbs-->
+      <Breadcrumbs active="0" />
+      <h2 class="subtitle" v-if="myStoryItems.length == 0">
+        There are no story items yet. Use the links above to add items to this
+        story.
+      </h2>
+      <StoryItemsGrid v-bind:storyItems="myStoryItems" v-bind:admin="true" />
+    </div>
+  </div>
 </template>
 <script>
-import axios from 'axios';
-import StoryItemsGrid from '@/components/StoryItemsGrid';
-import Breadcrumbs from '@/components/Breadcrumbs';
+import axios from "axios";
+import StoryItemsGrid from "@/components/StoryItemsGrid";
+import Breadcrumbs from "@/components/Breadcrumbs";
 export default {
-	components: {
-		StoryItemsGrid,
-		Breadcrumbs,
-	},
-	data() {
-		return {
-			myStoryItems: [],
-			nodes: [],
-			connections: [],
-			editable: true,
-		};
-	},
+  components: {
+    StoryItemsGrid,
+    Breadcrumbs,
+  },
+  data() {
+    return {
+      myStoryItems: [],
+      nodes: [],
+      connections: [],
+      editable: true,
+    };
+  },
 
-	created() {
-		axios
-			.post('/api/getAdminStoryItems', { id: this.$route.params.id })
-			.then((response) => {
-				if (response.status === 200) {
-					this.myStoryItems = response.data;
-				} else {
-					this.uploadError = true;
-				}
-			})
-			.catch((err) => {
-				this.uploadError = true;
-				console.error(err);
-			});
-	},
+  created() {
+    axios
+      .post("/api/getAdminStoryItems", { id: this.$route.params.id })
+      .then((response) => {
+        if (response.status === 200) {
+          this.myStoryItems = response.data;
+        } else {
+          this.uploadError = true;
+        }
+      })
+      .catch((err) => {
+        this.uploadError = true;
+        console.error(err);
+      });
+  },
 };
 </script>
